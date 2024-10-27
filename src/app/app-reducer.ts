c6c22ef655ecdd3332ffa64623fff59f0cbdbfad
vTarlettskiy <v.tarlettskiy@gmail.com>
2024-10-27 16:06:13 +0300

    start

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,18 @@
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+  status: 'idle' as RequestStatusType,
+  error: null as string | null,
+}
+
+type AppStateType = typeof initialState
+
+export const appReducer = (state: AppStateType = initialState, action: ActionsType): AppStateType => {
+  switch (action.type) {
+    case 'APP/SET-STATUS':
+    default:
+      return state
+  }
+}
+
+type ActionsType = any
